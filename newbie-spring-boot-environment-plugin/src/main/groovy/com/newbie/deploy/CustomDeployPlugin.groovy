37904a9a580d3e77c6ff5b6d6155e3eb75ec98af
halower <121625933@qq.com>
2019-06-12 22:06:39 +0800

    添加部署环境切换插件

---
+++ newbie-spring-boot-environment-plugin/src/main/groovy/com/newbie/deploy/CustomDeployPlugin.groovy
@@ -0,0 +1,109 @@
+package com.newbie.deploy
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+/**
+ * @Author: 谢海龙
+ * @Date: 2019/6/11 20:32
+ * @Description
+ */
+class CustomDeployPlugin implements Plugin<Project> {
+    def devSuffix = "-dev"
+    def applicationNameKey = "spring.application.name"
+    def dubboVersionKey = "dubbo.provider.version"
+    def bootstrap = new File('src/main/resources/bootstrap.properties')
+    def application = new File('src/main/resources/application.properties')
+    @Override
+    void apply(Project project) {
+        project.task('单机模式') {
+            doLast {
+                def updatedLocalBootstrap = []
+                bootstrap.readLines().each {
+                    def line ->
+                        def isChanged = line.startsWith(applicationNameKey) && line.endsWith(devSuffix)
+                        updatedLocalBootstrap.add(isChanged ? line[0..-5]: line)
+
+                }
+                println(updatedLocalBootstrap)
+                bootstrap.delete()
+                bootstrap.createNewFile()
+                bootstrap.withWriter { def writer ->
+                    updatedLocalBootstrap.each {
+                        line -> writer.writeLine line
+                    }
+                }
+                application.withWriter { def writer ->
+                    writer.writeLine "spring.profiles.active=local"
+                }
+            }
+        }
+        project.task('联调模式') {
+            doLast {
+                def updatedBootstrap = []
+                bootstrap.readLines().each {
+                    def line ->
+                        def isChanged = line.startsWith(applicationNameKey) && !line.endsWith(devSuffix)
+                        updatedBootstrap.add(isChanged ? line+ devSuffix: line)
+                }
+                bootstrap.delete()
+                bootstrap.createNewFile()
+                bootstrap.withWriter { def writer ->
+                    updatedBootstrap.each {
+                        line -> writer.writeLine line
+                    }
+                }
+
+                def random = new Random()
+                application.withWriter { def writer ->
+                    writer.writeLine "spring.profiles.active=dev"
+                    writer.writeLine dubboVersionKey + "=" + random.nextInt(3) + ":" + random.nextInt(10)  + ":" +random.nextInt(15)
+                }
+            }
+        }
+        project.task('内网模式') {
+            doLast {
+                def updatedLocalBootstrap = []
+                bootstrap.readLines().each {
+                    def line ->
+                        def isChanged = line.startsWith(applicationNameKey) && line.endsWith(devSuffix)
+                        updatedLocalBootstrap.add(isChanged ? line[0..-5]: line)
+
+                }
+                println(updatedLocalBootstrap)
+                bootstrap.delete()
+                bootstrap.createNewFile()
+                bootstrap.withWriter { def writer ->
+                    updatedLocalBootstrap.each {
+                        line -> writer.writeLine line
+                    }
+                }
+                application.withWriter { def writer ->
+                    writer.writeLine "spring.profiles.active=dev"
+                }
+            }
+        }
+        project.task('测试模式') {
+            doLast {
+                def updatedLocalBootstrap = []
+                bootstrap.readLines().each {
+                    def line ->
+                        def isChanged = line.startsWith(applicationNameKey) && line.endsWith(devSuffix)
+                        updatedLocalBootstrap.add(isChanged ? line[0..-5]: line)
+
+                }
+                println(updatedLocalBootstrap)
+                bootstrap.delete()
+                bootstrap.createNewFile()
+                bootstrap.withWriter { def writer ->
+                    updatedLocalBootstrap.each {
+                        line -> writer.writeLine line
+                    }
+                }
+                application.withWriter { def writer ->
+                    writer.writeLine "spring.profiles.active=test"
+                }
+            }
+        }
+    }
+}
