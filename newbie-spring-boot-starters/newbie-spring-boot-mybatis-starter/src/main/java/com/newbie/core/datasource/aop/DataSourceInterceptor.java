cf561b107b97eb8b64d03c9da244970cf935f6b8
halower <121625933@qq.com>
2019-10-14 09:37:32 +0800

    dubbo 过滤器配置

--- newbie-spring-boot-starters/newbie-spring-boot-mybatis-starter/src/main/java/com/newbie/core/datasource/aop/DataSourceInterceptor.java
+++ newbie-spring-boot-starters/newbie-spring-boot-mybatis-starter/src/main/java/com/newbie/core/datasource/aop/DataSourceInterceptor.java
@@ -16,52 +16,53 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
-package com.newbie.core.datasource.aop;
-
-import com.newbie.constants.NewbieBootInfraConstants;
-import com.newbie.core.datasource.DatabaseSourceKey;
-import com.newbie.core.datasource.DynamicDataSourceContextHolder;
-import com.newbie.core.exception.BusinessException;
-import lombok.extern.log4j.Log4j2;
-import lombok.var;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Arrays;
-import java.util.Locale;
-
-/**
- * @Author: halower
- * @Date: 2019/8/2 9:24
- * @Description
- */
-@Log4j2
-@Component
-@Configuration
-public class DataSourceInterceptor extends HandlerInterceptorAdapter {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        var wrf = NewbieBootInfraConstants.WRITE_READ_TYPE_FLAG;
-        DatabaseSourceKey databaseSourceKey = DatabaseSourceKey.DEFAULT;
-        if(request.getHeader(wrf)!=null && !request.getHeader(wrf).equals("")) {
-            String wrfName = request.getHeader(wrf).trim();
-            var legal_db_request =  Arrays.asList(NewbieBootInfraConstants.LEGAL_DB_REQUEST.split(","));
-            if(!legal_db_request.contains(wrfName.toUpperCase())) {
-                throw new BusinessException("当前请求库不存在");
-            }
-            databaseSourceKey =DatabaseSourceKey.valueOf(wrfName.toUpperCase(Locale.ENGLISH));
-        }
-        DynamicDataSourceContextHolder.setDataSourceType(databaseSourceKey);
-        return super.preHandle(request, response, handler);
-    }
-
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        DynamicDataSourceContextHolder.clearDataSourceType();
-        super.afterCompletion(request, response, handler, ex);
-    }
-}
+//package com.newbie.core.datasource.aop;
+//
+//import com.newbie.constants.NewbieBootInfraConstants;
+//import com.newbie.core.datasource.DatabaseSourceKey;
+//import com.newbie.core.datasource.DynamicDataSourceContextHolder;
+//import com.newbie.core.exception.BusinessException;
+//import lombok.extern.log4j.Log4j2;
+//import lombok.var;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.stereotype.Component;
+//import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+//
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.util.Arrays;
+//import java.util.Locale;
+//
+///**
+// * @Author: halower
+// * @Date: 2019/8/2 9:24
+// * @Description
+// */
+//@Log4j2
+//@Component
+//@Configuration
+//public class DataSourceInterceptor extends HandlerInterceptorAdapter {
+//    @Override
+//    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//        var wrf = NewbieBootInfraConstants.WRITE_READ_TYPE_FLAG;
+//        DatabaseSourceKey databaseSourceKey;
+//        if(request.getHeader(wrf)!=null && !request.getHeader(wrf).equals("")) {
+//            String wrfName = request.getHeader(wrf).trim();
+//            var legal_db_request =  Arrays.asList(NewbieBootInfraConstants.LEGAL_DB_REQUEST.split(","));
+//            if(!legal_db_request.contains(wrfName.toUpperCase())) {
+//                throw new BusinessException("当前请求库不存在");
+//            }
+//            databaseSourceKey =DatabaseSourceKey.valueOf(wrfName.toUpperCase(Locale.ENGLISH));
+//            DynamicDataSourceContextHolder.setDataSourceType(databaseSourceKey);
+//        } else {
+//            DynamicDataSourceContextHolder.setDataSourceType(DatabaseSourceKey.DEFAULT);
+//        }
+//        return super.preHandle(request, response, handler);
+//    }
+//
+//    @Override
+//    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+//        DynamicDataSourceContextHolder.clearDataSourceType();
+//        super.afterCompletion(request, response, handler, ex);
+//    }
+//}
